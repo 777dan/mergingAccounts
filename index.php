7dc3a4e602e2426642c34d94740c7beee9d63460
777dan <danya.nikitin7@gmail.com>
2022-09-28 08:18:35 +0300

    second commit

--- index.php
+++ index.php
@@ -1,13 +1,11 @@
 <?php
 $users1 = ["John" => "qwerty", "Nicole" => "asdf", "Mark" => "ww"];
 $users2 = ["Joan" => "1234", "Mark" => "poiu", "Nicole" => "ggg"];
-$intersect1 = array_intersect_key($users1, $users2);
-$intersect2 = array_intersect_key($users2, $users1);
-print_r(array_merge_recursive($intersect1, $intersect2));
-$diff1 = (array_diff_key($users1, $users2));
-$diff2 = (array_diff_key($users2, $users1));
-// print_r(array_merge_recursive($diff1, $diff2));
-$users1 = $diff1;
-$users2 = $diff2;
-// print_r($users1);
-// print_r($users2);
+$users = array_merge_recursive($users1, $users2);
+$keys = array_intersect_key($users1, $users2);
+$additional = array_intersect_key($users, $keys);
+$result = (array_diff_key($users, $additional));
+// print_r($result);
+// print_r($users);
+// print_r($keys);
+print_r($additional);
